93143d6b3c0aee45b67b242c30127bbe91adf7c3
Yiqiao Wang <yiqiao.wang4@mail.mcgill.ca>
2023-04-07 01:52:54 -0400

    Update config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  devServer: {
+    port: 3000,
+  },
   transpileDependencies: true,
+  publicPath: "/",
 });
