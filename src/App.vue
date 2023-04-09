819691389bec30f425c729e8b66807461aade1b5
Yiqiao Wang <yiqiao.wang4@mail.mcgill.ca>
2023-04-08 22:11:26 -0400

    Modify imports

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <router-view></router-view>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+<script setup></script>
 
 <style>
 #app {
@@ -21,6 +11,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-left: 100px;
 }
 </style>
