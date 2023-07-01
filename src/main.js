55145005a360f06756927db1fbeef4126cbb9469
Ying Fei Zang <yf.zang09@gmail.com>
2023-07-01 18:47:28 -0400

    implement footer and working on membership

--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import App from './App.vue'
 import store from './store'
 import router from './router.js'
 
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+
 createApp(App).use(store).use(router).mount('#app')
