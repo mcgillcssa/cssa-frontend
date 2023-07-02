c583675c69770649b751fd3a3fc6934b76576f25
StevenTian0 <zhenyuan.tian@mail.mcgill.ca>
2023-07-01 21:02:13 -0400

    [feat] change eventpaage

--- src/components/RecentEvents.vue
+++ src/components/RecentEvents.vue
@@ -8,8 +8,10 @@
       <div v-for="(item, index) in items" :key="index" class="carousel-item">
         <div class="carousel-item-content">
           <div class="event-heading">
-            <h3>{{ item.eventName }}</h3>
-            <span class="event-date">{{ item.eventStartDate }}</span>
+            <div style="event-heading-info">
+              <span class="event-date">{{ formatDate(item.eventStartDate) }}</span>
+              <h3 class="event-name">{{ item.eventName }}</h3>
+            </div>
           </div>
           <p class="event-description">{{ item.eventDescription }}</p>
         </div>
@@ -25,6 +27,15 @@ export default {
   data() {
     return {
       items: []
+    }
+  },
+
+  methods: {
+    formatDate(dateString) {
+      const date = new Date(dateString)
+      const month = (date.getMonth() + 1).toString().padStart(2, '0')
+      const day = date.getDate().toString().padStart(2, '0')
+      return `${month}${day}`
     }
   },
 
@@ -57,18 +68,19 @@ body {
 
 .recent-events-title {
   width: 100%;
-  height: 100px;
+  height: 200px;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  font-size: 2vw;
 }
 
 .recent-events-title .zhs-title {
   font-family: 'Raleway';
   font-style: normal;
   font-weight: 400;
-  font-size: 1em;
+  font-size: 10em;
   letter-spacing: 0.175em;
   line-height: 0.8em;
   color: #ff6c6c;
@@ -77,14 +89,10 @@ body {
 .recent-events-title .en-title {
   font-family: 'Raleway';
   font-style: normal;
-  font-weight: 700;
+  font-weight: 800;
   font-size: 1.75em;
   line-height: 0.8em;
-  font-size: 5vw;
-}
-
-.recent-events-title .en-title {
-  font-size: 5vw;
+  font-size: 8vw;
 }
 
 #carousel {
@@ -109,26 +117,32 @@ body {
   width: 60%;
 }
 
-.carousel-item-content .event-heading h3 {
-  font-size: 2vw;
-}
-
-.carousel-item-content .event-date {
-  font-size: 2vw;
-}
-
-.carousel-item-content .event-description {
-  font-size: 2vw;
-}
-
 .event-image {
   width: 40%;
   max-width: 800px;
 }
 
 @media (max-width: 1200px) {
+  ::v-deep(.event-name) {
+    font-size: 40px; /* change to fixed size */
+  }
+
+  ::v-deep(.event-date) {
+    font-size: 100px; /* change to fixed size */
+  }
+
+  ::v-deep(.carousel-item-content .event-heading h3) {
+    font-size: 30px;
+  }
+
+  ::v-deep(.carousel-item-content .event-description) {
+    font-size: 25px; /* change to fixed size */
+    padding-left: 0;
+    padding-right: 0;
+  }
   ::v-deep .carousel-item {
     flex-direction: column;
+    align-items: center;
   }
 
   ::v-deep .carousel-item:nth-child(even) {
@@ -143,16 +157,16 @@ body {
   ::v-deep .recent-events-title .zhs-title,
   ::v-deep .recent-events-title .en-title,
   ::v-deep .event-heading h3 {
-    font-size: 1rem;
-  }
-
-  ::v-deep .event-date {
     font-size: 2rem;
   }
 
+  ::v-deep .event-heading .event-date {
+    font-size: 4rem;
+  }
+
   ::v-deep .event-description {
-    padding-left: 0rem;
-    padding-right: 0rem;
+    padding-left: 7rem;
+    padding-right: 5rem;
   }
 }
 
@@ -166,14 +180,23 @@ body {
   }
 
   ::v-deep .event-heading h3 {
-    font-size: 4rem;
+    font-size: 50px;
   }
 
-  ::v-deep .event-date {
-    font-size: 6rem;
+  ::v-deep .event-heading .event-date {
+    font-size: 7rem;
+  }
+
+  .carousel-item {
+    display: flex;
+    justify-content: space-between;
+    align-items: center; /* Add this */
+    padding: 1rem;
+    margin: 0 auto;
   }
 
   ::v-deep .event-description {
+    font-size: 30px;
     padding-left: 7rem;
     padding-right: 5rem;
   }
