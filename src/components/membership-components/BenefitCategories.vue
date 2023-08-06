dbf3c9fdb0b53e939a2e586dedc2b5946ee40d83
Yiqiao Wang <yiqiao.wang4@mail.mcgill.ca>
2023-08-06 02:10:36 -0400

    [fix] Fix styling broken by refactoring PR

--- src/components/membership-components/BenefitCategories.vue
+++ src/components/membership-components/BenefitCategories.vue
@@ -66,81 +66,121 @@ export default {
 }
 </script>
 
-<style scoped>
-.carousel {
+<style>
+body {
+  font-size: 24px;
+}
+.gradient-stripe {
+  width: 100%;
+  height: 24px;
+  background: linear-gradient(
+    90deg,
+    #ffc6b4 0.67%,
+    #ffa7d1 14.09%,
+    #ad87cb 39.63%,
+    #8986ed 68.36%,
+    #4f78c9 100%
+  );
+}
+.title-container {
+  width: 100%;
+  height: 150px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #eaebf6;
+  margin: 0;
+  padding: 0;
+}
+.title-container h1 {
   position: relative;
+  background: #8987cb;
+  text-align: center;
+  margin: 0 auto;
+  padding: 5px 0 0 0;
+  width: 600px;
+  height: 75px;
+  color: #fff;
+  letter-spacing: 2px;
+  font-weight: 600;
+  border-radius: 20px;
+  font-size: 2em;
+}
+.title-container h1::before,
+.title-container h1::after {
+  content: ''; /* this is necessary for the pseudo-element to be shown */
+  width: 10px; /* width of the square */
+  height: 10px; /* height of the square */
+  background-color: #7a65a3; /* color of the square */
+  position: absolute; /* position it absolutely with respect to the h1 element */
+  top: 50%; /* center it vertically */
+  transform: translateY(-50%); /* perfectly center it vertically */
+}
+.title-container h1::before {
+  left: -15px; /* position it to the left of the h1 */
+}
+.title-container h1::after {
+  right: -15px; /* position it to the right of the h1 */
+}
+.categories-list {
+  margin: 0;
+  padding: 0 0 50px 0;
+  display: flex;
+  flex-wrap: wrap; /* Wrap the columns if they don't fit the container width */
+  background: #eaebf6;
+  min-height: calc(100vh - 48px);
+}
+.column {
+  margin: 0;
+  flex: 1;
   display: flex;
   flex-direction: column;
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-}
-
-.carousel img {
-  width: 100%;
   height: auto;
-  max-height: 300px;
-  object-fit: cover;
-  display: block;
 }
 
-.control {
-  position: absolute;
-  z-index: 1;
-  transform: translateY(-50%);
-  transition: transform 0.3s ease;
-  cursor: pointer;
-}
-
-/* Apply hover effects only for devices that support hover */
-@media (hover: hover) {
-  .control:hover {
-    transform: translateY(-50%) scale(1.2);
+/* Three Columns for Large Screens (for screens larger than 1200px) */
+@media (min-width: 1200px) {
+  .column {
+    flex: 0 0 33.33%; /* flex-grow, flex-shrink, flex-basis */
+    max-width: 33.33%;
   }
 }
-
-.control:active {
-  transform: translateY(-50%) scale(0.9);
+/* Two Columns for Medium Screens (for screens between 600px and 1199px) */
+@media (min-width: 600px) and (max-width: 1199px) {
+  body {
+    font-size: 20px;
+  }
+  .column {
+    flex: 0 0 50%;
+    max-width: 50%;
+  }
+  .title-container {
+    height: 125px;
+  }
+  .title-container h1 {
+    padding: 3px 0 0 0;
+    width: 500px;
+    height: 60px;
+  }
 }
-
-.previous {
-  left: 10px;
-}
-
-.next {
-  right: 10px;
-}
-
-.slide-selector {
-  margin-top: 5px;
-  padding-top: 5px;
-  padding-bottom: 10px;
-  background-color: #ffffff;
-  bottom: 10px;
-  display: flex;
-  justify-content: center;
-  width: 100%;
-}
-
-/* Updated .selector class */
-.selector {
-  height: 4px; /* Make the line thicker */
-  margin: 0 5px;
-  background-color: rgb(255, 255, 255);
-  border: #9c71c6 2px solid;
-  cursor: pointer;
-  transition: width 0.3s ease, border-radius 0.3s ease; /* Added border-radius transition */
-  border-radius: 2px; /* Make the ends rounded */
-}
-
-.selector.active {
-  background-color: #9c71c6;
-  border-radius: 2px; /* Keep the ends rounded when active */
-}
-
+/* Small devices */
 @media (max-width: 600px) {
-  .carousel img {
-    max-height: 180px;
+  body {
+    font-size: 16px;
+  }
+  .categories-list {
+    flex-direction: column; /* This will stack the columns vertically */
+  }
+  .column {
+    width: 100%;
+  }
+  .title-container {
+    height: 100px;
+  }
+  .title-container h1 {
+    padding: 2px 0 0 0;
+    width: 400px;
+    height: 50px;
   }
 }
 </style>
