5c5408dc9981e38f7c113ba71d8b0fc24c0aedca
Karencheenn <karen.chenlai@mail.mcgill.ca>
2024-09-02 13:42:19 +0800

    [style] Update NavBar design

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -55,18 +55,18 @@
   <ul class="dropdown" :class="{ open: isDropdownOpen }">
     <li v-if="isDropdownOpen">EVENTS
       <svg width="400" height="17" viewBox="0 0 699 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M7.34329e-05 8.99719C0.0190177 13.4154 3.61606 16.9818 8.0343 16.9628C12.4525 16.9439 16.0189 13.3468 15.9999 8.92859C15.981 4.51035 12.3839 0.94402 7.9657 0.962964C3.54746 0.981908 -0.0188708 4.57895 7.34329e-05 8.99719ZM8.00643 10.4629L699.006 7.50005L698.994 4.50007L7.99357 7.4629L8.00643 10.4629Z" fill="#4F3875"/>
+        <path d="M7.34329e-05 8.99719C0.0190177 13.4154 3.61606 16.9818 8.0343 16.9628C12.4525 16.9439 16.0189 13.3468 15.9999 8.92859C15.981 4.51035 12.3839 0.94402 7.9657 0.962964C3.54746 0.981908 -0.0188708 4.57895 7.34329e-05 8.99719ZM8.00643 10.4629L699.006 7.50005L698.994 4.50007L7.99357 7.4629L8.00643 10.4629Z" fill="#1A4F87"/>
       </svg>
     </li>
     <li v-if="isDropdownOpen">
       <router-link class="link-element" to="/membership">MEMBERSHIP</router-link>
       <svg width="400" height="17" viewBox="0 0 699 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M7.34329e-05 8.99719C0.0190177 13.4154 3.61606 16.9818 8.0343 16.9628C12.4525 16.9439 16.0189 13.3468 15.9999 8.92859C15.981 4.51035 12.3839 0.94402 7.9657 0.962964C3.54746 0.981908 -0.0188708 4.57895 7.34329e-05 8.99719ZM8.00643 10.4629L699.006 7.50005L698.994 4.50007L7.99357 7.4629L8.00643 10.4629Z" fill="#4F3875"/>
+        <path d="M7.34329e-05 8.99719C0.0190177 13.4154 3.61606 16.9818 8.0343 16.9628C12.4525 16.9439 16.0189 13.3468 15.9999 8.92859C15.981 4.51035 12.3839 0.94402 7.9657 0.962964C3.54746 0.981908 -0.0188708 4.57895 7.34329e-05 8.99719ZM8.00643 10.4629L699.006 7.50005L698.994 4.50007L7.99357 7.4629L8.00643 10.4629Z" fill="#1A4F87"/>
       </svg>
     </li>
     <li v-if="isDropdownOpen">SPONSORS
       <svg width="400" height="17" viewBox="0 0 699 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M7.34329e-05 8.99719C0.0190177 13.4154 3.61606 16.9818 8.0343 16.9628C12.4525 16.9439 16.0189 13.3468 15.9999 8.92859C15.981 4.51035 12.3839 0.94402 7.9657 0.962964C3.54746 0.981908 -0.0188708 4.57895 7.34329e-05 8.99719ZM8.00643 10.4629L699.006 7.50005L698.994 4.50007L7.99357 7.4629L8.00643 10.4629Z" fill="#4F3875"/>
+        <path d="M7.34329e-05 8.99719C0.0190177 13.4154 3.61606 16.9818 8.0343 16.9628C12.4525 16.9439 16.0189 13.3468 15.9999 8.92859C15.981 4.51035 12.3839 0.94402 7.9657 0.962964C3.54746 0.981908 -0.0188708 4.57895 7.34329e-05 8.99719ZM8.00643 10.4629L699.006 7.50005L698.994 4.50007L7.99357 7.4629L8.00643 10.4629Z" fill="#1A4F87"/>
       </svg>
     </li>
   </ul>
@@ -105,7 +105,7 @@ const toggleDropdown = () => {
   isDropdownOpen.value = !isDropdownOpen.value
 
   if (isDropdownOpen.value) {
-    barColor.value = '#33378c'
+    barColor.value = '#1A4F87'
     document.body.style.overflow = 'hidden'
   } else {
     barColor.value = '#ffffff'
@@ -250,7 +250,7 @@ const toggleDropdown = () => {
   position: fixed;
   width: 100vw;
   height: 200vh;
-  background-color: rgba(79,56,117,0.3);
+  background-color: rgba(227, 244, 255, 0.3);
   z-index: 10;
 }
 
@@ -270,12 +270,17 @@ const toggleDropdown = () => {
   justify-content: center;
   gap: 8vh;
 
-  background-color: #F5ECFF;
+  background-color: #E3F4FF;
 
   border-left-width: 10px;
   border-left-style: solid;
-  border-image: linear-gradient(to bottom, #FFC6B4, #FFA7D1, #AD87CB, #8986ED, #4F78C9) 1;
-
+  border-image: linear-gradient(to bottom,     #FFEE8F 10.28%,
+    #E3FBCF 20.63%,
+    #BBF0FA 42.36%,
+    #ABD9FF 56.45%,
+    #5DABF3 70.52%,
+    #448FD6 84.88%,
+    #3262BA 100%) 1;
   transform: translateX(100%); /* Start the dropdown off-screen */
   transition: transform 0.5s cubic-bezier(0.4, 0, 0.2, 1), opacity 0.5s ease;
 }
@@ -289,7 +294,7 @@ const toggleDropdown = () => {
   font-weight: 700;
   font-style: italic;
   text-indent: 5%;
-  color: #4F3875;
+  color: #1A4F87;
   display: flex;
   flex-direction: column;
   overflow: hidden;
